fa9140bf5e969c97c8df2667d6b19f9702deff9a
farhah din <farhahdin97@hotmail.com>
2022-01-06 22:46:01 +0000

    Declaring more variables

--- script.js
+++ script.js
@@ -1,28 +1,23 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-//Declaring variables 
+
+var passLength;
+var CLower;
+var CUpper;
+var CNumber;
+var CSpecial;
+var Choices;
+
 var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+// Var To Upper Case ------------
+var blankU = [];
+var toUpper = function (x) {
+  return x.toUpperCase();
+};
+upperCase = lowerCase.map(toUpper);
+//--------------------------------------
 var numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 var special = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", "\:", "\;", " < ", "=", " > ", " ? ", "@", "[", "\\", "]", " ^ ", "_", "`", "{", "|", "}", "~"];
-
-//Converting variable to uppercase
-var blankU= [];
-var convertToUpper = function (x) {
-  return x.convertToUpperCase();
-};
-upperCase = lowerCase.map(ConvertToUpper);
-
-//Generating password function
-function generatePassword() {
-    // Ask for the users Input if the user doesn't input it will show an alert message
-    passwordLength = prompt("How many characters would you like your password? Choose between 8 and 128");
-    console.log("Password length " + passwordLength);
-    
-    if(!passwordLength) {
-      alert("Required value");
-    }
-}
-
 
 // Write password to the #password input
 function writePassword() {
@@ -32,5 +27,35 @@ function writePassword() {
   passwordText.value = password;
 
 }
+
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+// Start Function
+function generatePassword() {
+  // Ask four user Input
+  passLength = prompt("How many characters would you like your password? Choose between 8 and 128");
+  console.log("Password length " + passLength);
+  
+  if(!passwordLength) {
+    alert("Required value");
+
+  }
+  
+
+  // Empty variable for the password lenght
+  var passwordBlank = [];
+  
+  // Loop for random selection
+  for (var i = 0; i < passwordLength; i++) {
+    var allChoices = userChoices[Math.floor(Math.random() * userChoices.length)];
+    passwordBlank.push(allChoices);
+    console.log(allChoices);
+  }
+
+  // Join and return the password 
+  var password = passwordBlank.join("");
+  console.log("Your Pasword is: " + password);
+  return password;
+  
+}
